27272d3bc348978635944e8c113c6c1c55ff25da
bishalsth <bishal.sth.3@gmail.com>
2022-11-19 12:25:36 +0545

    Create user

---
+++ src/main/java/com/blog/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.payloads.UserDto;
+import com.blog.services.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+//	POST- create users
+	@PostMapping("/")
+	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+		UserDto createdUser = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
+		
+	}
+	
+//	PUT - Update USers
+//	DELETE - DELETE USer
+//	GET - GET USERS
+}
